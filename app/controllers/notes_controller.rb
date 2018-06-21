64220aa58d6d995b0ebea3a21d8dae8f5a104f39
chrisjamesr <chrisjames.r@gmail.com>
2018-06-21 19:42:34 -0400

    build out create, index, show, update, and destroy actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,34 @@
 class NotesController < ApplicationController
+
+  def create
+    note = Note.new(notes_params)
+    if note.save
+      render json: note
+    end
+  end
+
+  def index
+    notes = Note.all
+    render json: notes
+  end
+
+  def show
+    note = Note.find(params[:id])
+    render json: note
+  end
+
+  def update
+    note = Note.update(note_params)
+    render json: note
+  end
+
+  def destroy
+    note = Note.find(params[:id])
+    note.destroy
+  private
+
+  def notes_params
+    params.require(:note).permit(:title, :content)
+  end
+
 end
