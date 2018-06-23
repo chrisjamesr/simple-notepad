a7e19970884448d32fff1327006c8348f07ff44d
chrisjamesr <chrisjames.r@gmail.com>
2018-06-23 11:41:32 -0400

    add status codes and messages to successful action responses

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,28 +3,44 @@ class NotesController < ApplicationController
   def create
     note = Note.new(notes_params)
     if note.save
-      render json: note
+      render json: {
+        status: 201,
+        note: note
+      }
     end
   end
 
   def index
     notes = Note.all
-    render json: notes
+    render json: {
+      status: 200, 
+      notes: notes
+    }
   end
 
   def show
     note = Note.find(params[:id])
-    render json: note
+    render json: {
+      status: 200,
+      note: note
+    }
   end
 
   def update
     note = Note.update(notes_params)
-    render json: note
+    render json: {
+      status: 200,
+      note: note
+    }
   end
 
   def destroy
     note = Note.find(params[:id])
     note.destroy
+    render json: {
+      status: 200,
+      message: "Successfully deleted note."
+    }
   end
 
   private
