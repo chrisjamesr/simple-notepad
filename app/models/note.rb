3d7dd81daaa5e2ec51866c07fcfd7a0219da573d
chrisjamesr <chrisjames.r@gmail.com>
2018-06-23 11:30:43 -0400

    add created and updated methods to reformat created_at and updated_at

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,19 @@
 class Note < ApplicationRecord
   validates :title, presence: {allow_blank: false}, uniqueness: {case_sensitive: false}
   validates :content, presence: true
+
+  attr_reader :created, :updated
+
+  def created
+    @created = format_date(DateTime.parse(self.created_at.to_s))
+  end
+
+  def updated
+    @updated = format_date(DateTime.parse(self.updated_at.to_s))
+  end
+
+  def format_date(date_object)
+    date_object.strftime("%_m-%d-%Y at%l:%M%P")
+  end
+
 end
